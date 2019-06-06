bf071c69f7f533c36f89d11424d4519af8c1a31c
dev2 <gregorybouyssou>
2019-06-06 17:01:54 +0200

    test1

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
+    
+    let char = document.querySelector('.char')
+    let vitesse = (1)
+    
+
+    window.addEventListener('keydown', function(e){
+        if(e.key === 'ArrowRight'){
+           
+            console.log('wesh k')
+        }
+        
+
+        if(e.key === 'ArrowLeft'){
+            console.log('wesh l')
+        }
+    })
+
+    
+})
